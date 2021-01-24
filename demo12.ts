1c5fe6382c5d4932a02fcefb8a3eefca7eb99c7e
will <iwillbe12138@163.com>
2021-01-25 01:27:47 +0800

    2021-01-25

--- demo12.ts
+++ demo12.ts
@@ -2,15 +2,27 @@
  * @Description:
  * @Author: iwillbe12138
  * @Date: 2021-01-17 23:43:31
- * @LastEditTime: 2021-01-17 23:46:23
+ * @LastEditTime: 2021-01-25 01:06:23
  * @LastEditors:
  */
 
 class Person {
-  public name: string;
+  // public name: string;
   //  构造函数
-  constructor(name: string) {
-    this.name = name;
+  // constructor(name: string) {
+  //   this.name = name;
+  // }
+  // constructor(public name: string) {}
+  // constructor() {} class 默认有构造函数  没写默认为空
+}
+
+class teacher extends Person {
+  constructor(public age: number) {
+    // super("will"); //构造函数 继承必须写super
+    super(); //构造函数 继承必须写super
   }
 }
-const person = new Person("will");
+// const person = new Person("will");
+const Teacher = new teacher(18);
+console.log(Teacher.age);
+// console.log(Teacher.name);
---
+++ demo13.ts
@@ -0,0 +1,33 @@
+// // /*
+// //  * @Description:get set 静态类
+// //  * @Author: iwillbe12138
+// //  * @Date: 2021-01-25 01:06:33
+// //  * @LastEditTime: 2021-01-25 01:19:24
+// //  * @LastEditors:
+// //  */
+// // class xiaoJieJie {
+// //   constructor(private _age: number) {}
+// //   get age() {
+// //     return this._age - 10;
+// //   }
+// //   set age(age: number) {
+// //     this._age = age + 3;
+// //   }
+// // }
+// // const daJiao = new xiaoJieJie(28);
+// // daJiao.age = 25;
+// // // console.log(daJiao._age) //访问不到
+// // console.log(daJiao.age);
+
+// class Girl {
+//   // sayLove() {
+//   //   return "I Love You";
+//   // }
+//   static sayLove() {
+//     return "I Love You";
+//   }
+// }
+
+// // const girl = new Girl();
+// // console.log(girl.sayLove());
+// console.log(Girl.sayLove()); //静态类 调用
